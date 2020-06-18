cf9749ab0d850f0fbde83aa510aca910be68281f
Patrick-santana <zonaabissal2016@gmail.com>
2020-06-18 09:36:39 -0300

    Upando App

---
+++ app/src/main/java/com/example/staraplicacao/Proposicao.kt
@@ -0,0 +1,7 @@
+package com.example.staraplicacao
+
+data class Proposicao(var id: Int,
+                      var uri: String,
+                      var siglaTipo: String,
+                      var ano: Int,
+                      var emenda: String)
